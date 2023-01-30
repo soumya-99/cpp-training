1b9b9bd0588992f7e7a19d01b2d759c00d9ec65a
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-01-30 23:28:27 +0530

    dma

---
+++ dma.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    // Heap (free store)
+    int* numbers = new int[10];
+    // free the memory
+    delete[] numbers;
+    numbers = nullptr; // reset the pointer
+
+    int* number = new int;
+    // free the memory
+    delete number;
+    number = nullptr; // reset the pointer
+
+    return 0;
+}
