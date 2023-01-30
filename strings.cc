093ab8d1f26afe836e9b55d62c8bf13a759d0ed5
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-01-31 00:57:24 +0530

    modifying strings

--- strings.cc
+++ strings.cc
@@ -10,18 +10,37 @@ int main(int argc, char const *argv[]) {
     // cout << "Your name: " << name;
 
     // excersize
-    string street, city, state, zip_code;
-    cout << "Enter street: ";
-    getline(cin, street);
-    cout << "Enter city: ";
-    getline(cin, city);
-    cout << "Enter state: ";
-    getline(cin, state);
-    cout << "Enter zip code: " << endl;
-    getline(cin, zip_code);
+    // string street, city, state, zip_code;
+    // cout << "Enter street: ";
+    // getline(cin, street);
+    // cout << "Enter city: ";
+    // getline(cin, city);
+    // cout << "Enter state: ";
+    // getline(cin, state);
+    // cout << "Enter zip code: " << endl;
+    // getline(cin, zip_code);
 
-    cout << "You live in " << street << ", " << city << ", " << state << "-"
-         << zip_code;
+    // cout << "You live in " << street << ", " << city << ", " << state << "-"
+    //      << zip_code;
 
+    // string name = "Soumya";
+    // name[0] = 's';
+    // cout << name.front() << endl;
+    // cout << name.back() << endl;
+    // string another = "bla bla";
+
+    // cout << name + another << endl;
+    // cout << std::boolalpha << name.empty() << endl;
+    // cout << name;
+
+    string name = "Soumyadeep";
+    name.insert(0, "Mr. ");
+    cout << name << endl;
+
+    name.erase(0, 6);
+    cout << name << endl;
+
+    name.replace(0, 3, "SOU");
+    cout << name << endl;
     return 0;
 }
