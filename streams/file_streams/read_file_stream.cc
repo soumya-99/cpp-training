af4c0e488f966c4277d7b1955087844c5b56c4a5
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-02-01 01:52:31 +0530

    reading csv with delim

---
+++ streams/file_streams/read_file_stream.cc
@@ -0,0 +1,39 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+struct Movie {
+    int id;
+    string title;
+    int year;
+};
+
+int main(int argc, char const *argv[]) {
+    ifstream file;
+    file.open("data.csv");
+
+    if (file.is_open()) {
+        string str;
+        getline(file, str);
+        
+        while (!file.eof()) {
+            getline(file, str, ',');
+            if (str.empty()) break;
+
+            Movie movie;
+            movie.id = stoi(str);
+            getline(file, str, ',');
+
+            movie.title = str;
+            getline(file, str);
+
+            movie.year = stoi(str);
+            cout << movie.title << endl;
+        }
+        file.close();
+    }
+
+    return 0;
+}
