091625f96d79d1e9bfcb22c55c5ff0ca9a991c1e
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-02-01 02:06:27 +0530

    read binaries

---
+++ streams/file_streams/read_binary.cc
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    ifstream file("numbers.bin");
+
+    if (file.is_open()) {
+        int number;
+        while (file.read(reinterpret_cast<char *>(&number), sizeof(number)))
+            cout << number << endl;
+        file.close();
+    }
+    return 0;
+}
