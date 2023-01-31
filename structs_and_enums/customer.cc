7794335f1a91beb0594a70bf69c5b8fcfbd2184a
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-01-31 20:26:52 +0530

    nested structs

--- structs_and_enums/customer.cc
+++ structs_and_enums/customer.cc
@@ -1,8 +1,16 @@
 #include <iostream>
 
+struct Address {
+    std::string street;
+    std::string city;
+    std::string state;
+    short zip;
+};
+
 struct Customer {
     int id;
     std::string name;
+    Address address;
     std::string email;
 };
 
@@ -13,12 +21,21 @@ int main(int argc, char const *argv[]) {
     std::cin >> customer.id;
     std::cout << "Enter customer name: ";
     std::cin >> customer.name;
+    std::cout << "Enter customer street: ";
+    std::cin >> customer.address.street;
+    std::cout << "Enter customer city: ";
+    std::cin >> customer.address.city;
+    std::cout << "Enter customer state: ";
+    std::cin >> customer.address.state;
+    std::cout << "Enter customer zip: ";
+    std::cin >> customer.address.zip;
     std::cout << "Enter customer Email: ";
     std::cin >> customer.email;
 
     std::cout << "Customer id: " << customer.id << std::endl;
     std::cout << "Customer name: " << customer.name << std::endl;
     std::cout << "Customer email: " << customer.email << std::endl;
+    std::cout << "Customer address: " << customer.address.street << ", " << customer.address.city << ", " << customer.address.state << ", " << customer.address.zip << std::endl;
 
     return 0;
 }
