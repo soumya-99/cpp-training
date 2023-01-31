f885556d5da568d931e1d41441e135c6dc8e6c44
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-01-31 20:51:17 +0530

    comparing structures

--- structs_and_enums/nested_structures.cc
+++ structs_and_enums/nested_structures.cc
@@ -11,17 +11,24 @@ struct Movie {
     std::string title;
     Date release_date;
     bool is_popular = true;
+
+    bool equals(const Movie& movie) {
+        return title == movie.title && release_date.year == movie.release_date.year && release_date.month == movie.release_date.month && release_date.day == movie.release_date.day;
+    }
 };
 
 int main(int argc, char const* argv[]) {
     std::vector<Movie> movies;
 
     movies.push_back({"The Revenant", {2015, 12, 25}});
+    movies.push_back({"The Revenant", {2015, 12, 25}});
     movies.push_back({"Pather Panchali", {1955, 8, 19}});
 
     for (const auto& movie : movies) {
         std::cout << movie.title << " (" << movie.release_date.year << ")" << std::endl;
     }
+
+    std::cout << movies[0].equals(movies[1]) << std::endl;
 
     return 0;
 }
