25bb699baa756d6772f94586841e07e41a0b61cb
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-02-01 01:26:48 +0530

    reusable stream buffer from scratch

--- streams/main.cc
+++ streams/main.cc
@@ -3,15 +3,26 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    cout << "First: ";
-    int first;
-    cin >> first;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    cout << "Second: ";
-    int second;
-    cin >> second;
+int get_number(const string& prompt) {
+    int number;
+    while (true) {
+        cout << prompt;
+        cin >> number;
+        if (cin.fail()) {
+            cout << "Error: Invalid input" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        } else {
+            break;
+        }
+    }
+    return number;
+}
+
+int main(int argc, char const *argv[]) {
+    int first = get_number("First: ");
+    int second = get_number("Second: ");
+    
     cout << "First: " << first << " Second: " << second << endl;
     return 0;
 }
