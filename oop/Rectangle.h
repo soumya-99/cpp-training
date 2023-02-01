c0a50060a80a0c383f7ffecdcd8e0ccd0b669405
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-02-01 18:20:08 +0530

    setup ready for oop

---
+++ oop/Rectangle.h
@@ -0,0 +1,15 @@
+// #ifndef RECTANGLE_H
+// #define RECTANGLE_H
+
+// #pragma once
+
+class Rectangle {
+   public:
+    int width;
+    int height;
+
+    void draw();
+    int get_area();
+};
+
+// #endif
