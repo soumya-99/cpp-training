baeca047895f7c41e78118ec2dba96b163bc38bd
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-03-19 17:19:36 +0530

    added fibonacci O(1)

---
+++ basics/fibonacci.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double fibonacci(int index) {
+    double phi = (1 + sqrt(5)) / 2;
+    return round(pow(phi, index) / sqrt(5));
+}
+
+int main(int argc, char const *argv[])
+{
+    double fibo = fibonacci(8);
+    cout << "Fibonacci: " << fibo << endl;
+    return 0;
+}
